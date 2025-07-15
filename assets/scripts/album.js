2ed8e954b6919df6e34bed4702294865aed981da
PernaGiuseppe <giuseppeperna97@hotmail.com>
2025-07-15 16:06:52 +0200

    buttons updates

---
+++ assets/scripts/album.js
@@ -0,0 +1,70 @@
+// Effetto hover
+document.getElementById('heartBtn').addEventListener('mouseenter', function () {
+  const icon = this.querySelector('i')
+  if (!icon.classList.contains('text-success')) {
+    icon.classList.remove('text-white-50')
+    icon.classList.add('text-success') // Verde Bootstrap
+  }
+})
+
+document.getElementById('heartBtn').addEventListener('mouseleave', function () {
+  const icon = this.querySelector('i')
+  if (!this.hasAttribute('data-active')) {
+    icon.classList.remove('text-success')
+    icon.classList.add('text-white-50')
+  }
+})
+
+// Effetto click (rimane verde)
+document.getElementById('heartBtn').addEventListener('click', function () {
+  const icon = this.querySelector('i')
+  if (this.hasAttribute('data-active')) {
+    // Disattiva
+    this.removeAttribute('data-active')
+    icon.classList.remove('text-success')
+    icon.classList.add('text-white-50')
+  } else {
+    // Attiva
+    this.setAttribute('data-active', 'true')
+    icon.classList.remove('text-white-50')
+    icon.classList.add('text-success')
+  }
+})
+document
+  .getElementById('download-icon')
+  .addEventListener('mouseenter', function () {
+    const icon = this.querySelector('i')
+    if (!icon.classList.contains('text-light')) {
+      icon.classList.remove('text-white-50')
+      icon.classList.add('text-light') // Verde Bootstrap
+    }
+  })
+
+document
+  .getElementById('download-icon')
+  .addEventListener('mouseleave', function () {
+    const icon = this.querySelector('i')
+    if (!this.hasAttribute('data-active')) {
+      icon.classList.remove('text-light')
+      icon.classList.add('text-white-50')
+    }
+  })
+document
+  .getElementById('dots-icon')
+  .addEventListener('mouseenter', function () {
+    const icon = this.querySelector('i')
+    if (!icon.classList.contains('text-light')) {
+      icon.classList.remove('text-white-50')
+      icon.classList.add('text-light') // Verde Bootstrap
+    }
+  })
+
+document
+  .getElementById('dots-icon')
+  .addEventListener('mouseleave', function () {
+    const icon = this.querySelector('i')
+    if (!this.hasAttribute('data-active')) {
+      icon.classList.remove('text-light')
+      icon.classList.add('text-white-50')
+    }
+  })
