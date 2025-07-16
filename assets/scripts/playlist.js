76ce3927ee37b30c29ae2a146a842512613b6323
PernaGiuseppe <giuseppeperna97@hotmail.com>
2025-07-16 12:30:42 +0200

    playlist updates

--- assets/scripts/playlist.js
+++ assets/scripts/playlist.js
@@ -1,164 +1,448 @@
 const playlists = [
   {
     id: 1,
-    titl: "Playlist Rock",
-    description: "Una selezione dei migliori brani rock.",
+    titl: 'Playlist Rock',
+    description: 'Una selezione dei migliori brani rock.',
     songs: 42,
-    image: "./assets/imgs/main/image-3.jpg",
+    image: './assets/imgs/main/image-3.jpg',
     tracks: [
-      { title: "Bohemian Rhapsody", artist: "Queen", duration: "5:55" },
-      { title: "Back in Black", artist: "AC/DC", duration: "4:15" },
-      { title: "Sweet Child O' Mine", artist: "Guns N' Roses", duration: "5:56" },
-      { title: "Hotel California", artist: "Eagles", duration: "6:30" },
-      { title: "Smoke on the Water", artist: "Deep Purple", duration: "5:40" },
-      { title: "Highway to Hell", artist: "AC/DC", duration: "3:28" },
-      { title: "Born to Be Wild", artist: "Steppenwolf", duration: "3:30" },
-      { title: "Enter Sandman", artist: "Metallica", duration: "5:32" },
-      { title: "Paradise City", artist: "Guns N' Roses", duration: "6:45" },
-      { title: "Whole Lotta Love", artist: "Led Zeppelin", duration: "5:34" }
-    ]
+      {
+        title: 'Bohemian Rhapsody',
+        artist: 'Queen',
+        duration: '5:55',
+        album: 'A Night at the Opera',
+      },
+      {
+        title: 'Back in Black',
+        artist: 'AC/DC',
+        duration: '4:15',
+        album: 'Back in Black',
+      },
+      {
+        title: "Sweet Child O' Mine",
+        artist: "Guns N' Roses",
+        duration: '5:56',
+        album: 'Appetite for Destruction',
+      },
+      {
+        title: 'Hotel California',
+        artist: 'Eagles',
+        duration: '6:30',
+        album: 'Hotel California',
+      },
+      {
+        title: 'Smoke on the Water',
+        artist: 'Deep Purple',
+        duration: '5:40',
+        album: 'Machine Head',
+      },
+      {
+        title: 'Highway to Hell',
+        artist: 'AC/DC',
+        duration: '3:28',
+        album: 'Highway to Hell',
+      },
+      {
+        title: 'Born to Be Wild',
+        artist: 'Steppenwolf',
+        duration: '3:30',
+        album: 'Steppenwolf',
+      },
+      {
+        title: 'Enter Sandman',
+        artist: 'Metallica',
+        duration: '5:32',
+        album: 'Metallica (Black Album)',
+      },
+      {
+        title: 'Paradise City',
+        artist: "Guns N' Roses",
+        duration: '6:45',
+        album: 'Appetite for Destruction',
+      },
+      {
+        title: 'Whole Lotta Love',
+        artist: 'Led Zeppelin',
+        duration: '5:34',
+        album: 'Led Zeppelin II',
+      },
+    ],
   },
   {
     id: 2,
-    titl: "Playlist Chill",
-    description: "Musica rilassante per ogni momento.",
+    titl: 'Playlist Chill',
+    description: 'Musica rilassante per ogni momento.',
     songs: 35,
-    image: "./assets/imgs/main/image-4.jpg",
+    image: './assets/imgs/main/image-4.jpg',
     tracks: [
-      { title: "Sunset Lover", artist: "Petit Biscuit", duration: "3:58" },
-      { title: "Weightless", artist: "Marconi Union", duration: "8:00" },
-      { title: "Night Owl", artist: "Galimatias", duration: "3:55" },
-      { title: "Cold Little Heart", artist: "Michael Kiwanuka", duration: "9:58" },
-      { title: "Lost in Japan", artist: "Shawn Mendes", duration: "3:21" },
-      { title: "Lovely", artist: "Billie Eilish & Khalid", duration: "3:20" },
-      { title: "Ocean Eyes", artist: "Billie Eilish", duration: "3:20" },
-      { title: "Location", artist: "Khalid", duration: "3:39" },
-      { title: "Talk", artist: "Khalid", duration: "3:17" },
-      { title: "Better", artist: "Khalid", duration: "3:49" }
-    ]
+      {
+        title: 'Sunset Lover',
+        artist: 'Petit Biscuit',
+        duration: '3:58',
+        album: 'Presence',
+      },
+      {
+        title: 'Weightless',
+        artist: 'Marconi Union',
+        duration: '8:00',
+        album: 'Weightless',
+      },
+      {
+        title: 'Night Owl',
+        artist: 'Galimatias',
+        duration: '3:55',
+        album: 'Blowsom EP',
+      },
+      {
+        title: 'Cold Little Heart',
+        artist: 'Michael Kiwanuka',
+        duration: '9:58',
+        album: 'Love & Hate',
+      },
+      {
+        title: 'Lost in Japan',
+        artist: 'Shawn Mendes',
+        duration: '3:21',
+        album: 'Shawn Mendes',
+      },
+      {
+        title: 'Lovely',
+        artist: 'Billie Eilish & Khalid',
+        duration: '3:20',
+        album: 'dont smile at me',
+      },
+      {
+        title: 'Ocean Eyes',
+        artist: 'Billie Eilish',
+        duration: '3:20',
+        album: 'dont smile at me',
+      },
+      {
+        title: 'Location',
+        artist: 'Khalid',
+        duration: '3:39',
+        album: 'American Teen',
+      },
+      {
+        title: 'Talk',
+        artist: 'Khalid',
+        duration: '3:17',
+        album: 'Free Spirit',
+      },
+      { title: 'Better', artist: 'Khalid', duration: '3:49', album: 'Suncity' },
+    ],
   },
   {
     id: 3,
-    titl: "Playlist Rock",
-    description: "Una selezione dei migliori brani rock.",
+    titl: 'Playlist Rock',
+    description: 'Una selezione dei migliori brani rock.',
     songs: 42,
-    image: "./assets/imgs/main/image-5.jpg",
+    image: './assets/imgs/main/image-5.jpg',
     tracks: [
-      { title: "Paint It Black", artist: "The Rolling Stones", duration: "3:45" },
-      { title: "Come As You Are", artist: "Nirvana", duration: "3:38" },
-      { title: "Seven Nation Army", artist: "The White Stripes", duration: "3:52" },
-      { title: "Californication", artist: "Red Hot Chili Peppers", duration: "5:21" },
-      { title: "Creep", artist: "Radiohead", duration: "3:58" },
-      { title: "Karma Police", artist: "Radiohead", duration: "4:21" },
-      { title: "Black", artist: "Pearl Jam", duration: "5:43" },
-      { title: "Everlong", artist: "Foo Fighters", duration: "4:10" },
-      { title: "No One Knows", artist: "Queens of the Stone Age", duration: "4:38" },
-      { title: "My Hero", artist: "Foo Fighters", duration: "4:20" }
-    ]
+      {
+        title: 'Paint It Black',
+        artist: 'The Rolling Stones',
+        duration: '3:45',
+        album: 'Aftermath',
+      },
+      {
+        title: 'Come As You Are',
+        artist: 'Nirvana',
+        duration: '3:38',
+        album: 'Nevermind',
+      },
+      {
+        title: 'Seven Nation Army',
+        artist: 'The White Stripes',
+        duration: '3:52',
+        album: 'Elephant',
+      },
+      {
+        title: 'Californication',
+        artist: 'Red Hot Chili Peppers',
+        duration: '5:21',
+        album: 'Californication',
+      },
+      {
+        title: 'Creep',
+        artist: 'Radiohead',
+        duration: '3:58',
+        album: 'Pablo Honey',
+      },
+      {
+        title: 'Karma Police',
+        artist: 'Radiohead',
+        duration: '4:21',
+        album: 'OK Computer',
+      },
+      { title: 'Black', artist: 'Pearl Jam', duration: '5:43', album: 'Ten' },
+      {
+        title: 'Everlong',
+        artist: 'Foo Fighters',
+        duration: '4:10',
+        album: 'The Colour and the Shape',
+      },
+      {
+        title: 'No One Knows',
+        artist: 'Queens of the Stone Age',
+        duration: '4:38',
+        album: 'Songs for the Deaf',
+      },
+      {
+        title: 'My Hero',
+        artist: 'Foo Fighters',
+        duration: '4:20',
+        album: 'The Colour and the Shape',
+      },
+    ],
   },
   {
     id: 4,
-    titl: "Playlist Chill",
-    description: "Musica rilassante per ogni momento.",
+    titl: 'Playlist Chill',
+    description: 'Musica rilassante per ogni momento.',
     songs: 35,
-    image: "./assets/imgs/main/image-6.jpg",
+    image: './assets/imgs/main/image-6.jpg',
     tracks: [
-      { title: "Bloom", artist: "ODESZA", duration: "3:40" },
-      { title: "Intro", artist: "The xx", duration: "2:07" },
-      { title: "Electric", artist: "Alina Baraz feat. Khalid", duration: "3:45" },
-      { title: "Get You", artist: "Daniel Caesar", duration: "4:38" },
-      { title: "Pink + White", artist: "Frank Ocean", duration: "3:04" },
-      { title: "Retrograde", artist: "James Blake", duration: "3:43" },
-      { title: "Lost", artist: "Frank Ocean", duration: "3:54" },
-      { title: "Sunflower", artist: "Post Malone", duration: "2:38" },
-      { title: "Better Together", artist: "Jack Johnson", duration: "3:27" },
-      { title: "Banana Pancakes", artist: "Jack Johnson", duration: "3:12" }
-    ]
+      {
+        title: 'Bloom',
+        artist: 'ODESZA',
+        duration: '3:40',
+        album: 'In Return',
+      },
+      { title: 'Intro', artist: 'The xx', duration: '2:07', album: 'xx' },
+      {
+        title: 'Electric',
+        artist: 'Alina Baraz feat. Khalid',
+        duration: '3:45',
+        album: 'The Color of You',
+      },
+      {
+        title: 'Get You',
+        artist: 'Daniel Caesar',
+        duration: '4:38',
+        album: 'Freudian',
+      },
+      {
+        title: 'Pink + White',
+        artist: 'Frank Ocean',
+        duration: '3:04',
+        album: 'Blonde',
+      },
+      {
+        title: 'Retrograde',
+        artist: 'James Blake',
+        duration: '3:43',
+        album: 'Overgrown',
+      },
+      {
+        title: 'Lost',
+        artist: 'Frank Ocean',
+        duration: '3:54',
+        album: 'Channel Orange',
+      },
+      {
+        title: 'Sunflower',
+        artist: 'Post Malone',
+        duration: '2:38',
+        album: 'Spider-Man: Into the Spider-Verse',
+      },
+      {
+        title: 'Better Together',
+        artist: 'Jack Johnson',
+        duration: '3:27',
+        album: 'In Between Dreams',
+      },
+      {
+        title: 'Banana Pancakes',
+        artist: 'Jack Johnson',
+        duration: '3:12',
+        album: 'In Between Dreams',
+      },
+    ],
   },
   {
     id: 5,
-    titl: "Playlist Rock",
-    description: "Una selezione dei migliori brani rock.",
+    titl: 'Playlist Rock',
+    description: 'Una selezione dei migliori brani rock.',
     songs: 42,
-    image: "./assets/imgs/main/image-7.jpg",
+    image: './assets/imgs/main/image-7.jpg',
     tracks: [
-      { title: "Welcome to the Jungle", artist: "Guns N' Roses", duration: "4:31" },
-      { title: "The Pretender", artist: "Foo Fighters", duration: "4:29" },
-      { title: "Chop Suey!", artist: "System of a Down", duration: "3:30" },
-      { title: "Toxicity", artist: "System of a Down", duration: "3:39" },
-      { title: "B.Y.O.B.", artist: "System of a Down", duration: "4:15" },
-      { title: "In the End", artist: "Linkin Park", duration: "3:36" },
-      { title: "Numb", artist: "Linkin Park", duration: "3:05" },
-      { title: "Somebody Told Me", artist: "The Killers", duration: "3:17" },
-      { title: "Mr. Brightside", artist: "The Killers", duration: "3:43" },
-      { title: "Sex on Fire", artist: "Kings of Leon", duration: "3:23" }
-    ]
+      {
+        title: 'Welcome to the Jungle',
+        artist: "Guns N' Roses",
+        duration: '4:31',
+        album: 'Appetite for Destruction',
+      },
+      {
+        title: 'The Pretender',
+        artist: 'Foo Fighters',
+        duration: '4:29',
+        album: 'Echoes, Silence, Patience & Grace',
+      },
+      {
+        title: 'Chop Suey!',
+        artist: 'System of a Down',
+        duration: '3:30',
+        album: 'Toxicity',
+      },
+      {
+        title: 'Toxicity',
+        artist: 'System of a Down',
+        duration: '3:39',
+        album: 'Toxicity',
+      },
+      {
+        title: 'B.Y.O.B.',
+        artist: 'System of a Down',
+        duration: '4:15',
+        album: 'Mezmerize',
+      },
+      {
+        title: 'In the End',
+        artist: 'Linkin Park',
+        duration: '3:36',
+        album: 'Hybrid Theory',
+      },
+      {
+        title: 'Numb',
+        artist: 'Linkin Park',
+        duration: '3:05',
+        album: 'Meteora',
+      },
+      {
+        title: 'Somebody Told Me',
+        artist: 'The Killers',
+        duration: '3:17',
+        album: 'Hot Fuss',
+      },
+      {
+        title: 'Mr. Brightside',
+        artist: 'The Killers',
+        duration: '3:43',
+        album: 'Hot Fuss',
+      },
+      {
+        title: 'Sex on Fire',
+        artist: 'Kings of Leon',
+        duration: '3:23',
+        album: 'Only by the Night',
+      },
+    ],
   },
   {
     id: 6,
-    titl: "Playlist Chill",
-    description: "Musica rilassante per ogni momento.",
+    titl: 'Playlist Chill',
+    description: 'Musica rilassante per ogni momento.',
     songs: 35,
-    image: "./assets/imgs/main/image-8.jpg",
+    image: './assets/imgs/main/image-8.jpg',
     tracks: [
-      { title: "River", artist: "Leon Bridges", duration: "3:59" },
-      { title: "Breathe Me", artist: "Sia", duration: "4:35" },
-      { title: "Tenerife Sea", artist: "Ed Sheeran", duration: "4:00" },
-      { title: "All I Want", artist: "Kodaline", duration: "5:05" },
-      { title: "Skinny Love", artist: "Bon Iver", duration: "3:58" },
-      { title: "Holocene", artist: "Bon Iver", duration: "5:36" },
-      { title: "Re: Stacks", artist: "Bon Iver", duration: "6:41" },
-      { title: "Photograph", artist: "Ed Sheeran", duration: "4:18" },
-      { title: "Let It Go", artist: "James Bay", duration: "4:21" },
-      { title: "Say You Won't Let Go", artist: "James Arthur", duration: "3:31" }
-    ]
-  }
-];
+      {
+        title: 'River',
+        artist: 'Leon Bridges',
+        duration: '3:59',
+        album: 'Coming Home',
+      },
+      {
+        title: 'Breathe Me',
+        artist: 'Sia',
+        duration: '4:35',
+        album: 'Colour the Small One',
+      },
+      {
+        title: 'Tenerife Sea',
+        artist: 'Ed Sheeran',
+        duration: '4:00',
+        album: 'x (Multiply)',
+      },
+      {
+        title: 'All I Want',
+        artist: 'Kodaline',
+        duration: '5:05',
+        album: 'In a Perfect World',
+      },
+      {
+        title: 'Skinny Love',
+        artist: 'Bon Iver',
+        duration: '3:58',
+        album: 'For Emma, Forever Ago',
+      },
+      {
+        title: 'Holocene',
+        artist: 'Bon Iver',
+        duration: '5:36',
+        album: 'Bon Iver, Bon Iver',
+      },
+      {
+        title: 'Re: Stacks',
+        artist: 'Bon Iver',
+        duration: '6:41',
+        album: 'For Emma, Forever Ago',
+      },
+      {
+        title: 'Photograph',
+        artist: 'Ed Sheeran',
+        duration: '4:18',
+        album: 'x (Multiply)',
+      },
+      {
+        title: 'Let It Go',
+        artist: 'James Bay',
+        duration: '4:21',
+        album: 'Chaos and the Calm',
+      },
+      {
+        title: "Say You Won't Let Go",
+        artist: 'James Arthur',
+        duration: '3:31',
+        album: 'Back from the Edge',
+      },
+    ],
+  },
+]
 
-  //  Recupera l'ID dalla query string (?id=6)
-  const urlParams = new URLSearchParams(window.location.search);
-  const playlistId = parseInt(urlParams.get("id"));
+//  Recupera l'ID dalla query string (?id=6)
+const urlParams = new URLSearchParams(window.location.search)
+const playlistId = parseInt(urlParams.get('id'))
 
- const playlist = playlists.find(p => p.id === playlistId);
+const playlist = playlists.find((p) => p.id === playlistId)
 
-  if (playlist) {
-    document.getElementById("titl").textContent = playlist.titl;
-    document.getElementById("description").textContent = playlist.description;
-    document.getElementById("image").src = playlist.image;
-    document.getElementById("songs").textContent = `Contiene ${playlist.songs} brani`;
-  } else {
-    document.body.innerHTML = "Playlist non trovata.";
-  }
+if (playlist) {
+  document.getElementById('titl').textContent = playlist.titl
+  document.getElementById('description').textContent = playlist.description
+  document.getElementById('image').src = playlist.image
+  document.getElementById(
+    'songs'
+  ).textContent = `Contiene ${playlist.songs} brani`
+} else {
+  document.body.innerHTML = 'Playlist non trovata.'
+}
 
+//  Trova la playlist giusta
+const selectedPlaylist = playlists.find((pl) => pl.id === playlistId)
 
-  //  Trova la playlist giusta
-  const selectedPlaylist = playlists.find(pl => pl.id === playlistId);
+const container = document.getElementById('tracks-container')
 
-  const container = document.getElementById("tracks-container");
-
-  //  Inserisci i brani
-  if (selectedPlaylist) {
-    selectedPlaylist.tracks.forEach((track, index) => {
-      const row = document.createElement("div");
-      row.className = "row align-items-center p-3 border-bottom border-secondary border-opacity-10 track-item";
-      row.innerHTML = `
+//  Inserisci i brani
+if (selectedPlaylist) {
+  selectedPlaylist.tracks.forEach((track, index) => {
+    const row = document.createElement('div')
+    row.className =
+      'row align-items-center p-3 border-bottom border-secondary border-opacity-10 track-item'
+    row.innerHTML = `
         <div class="col-1 text-center text-white-50">${index + 1}</div>
         <div class="col-6">
           <div class="fw-normal text-white">${track.title}</div>
           <small class="text-white-50">${track.artist}</small>
         </div>
-        <div class="col-3 text-end text-white-50">${selectedPlaylist.title}</div>
+        <div class="col-3 text-end text-white-50">${track.album}</div>
         <div class="col-2 text-end text-white-50">${track.duration}</div>
-      `;
-      container.appendChild(row);
-    });
-  } else {
-    container.innerHTML = `
+      `
+    container.appendChild(row)
+  })
+} else {
+  container.innerHTML = `
       <div class="text-center text-white p-4">
         Playlist non trovata.
       </div>
-    `;
-  }
-
- 
-
+    `
+}
