656c1f1232ee1e6fccd6f0ab5320fb86550a47dd
Viktor Vasileski <viktor.vasileski1@gmail.com>
2025-07-17 16:58:47 +0200

    fixes artist page

--- assets/scripts/friends-tab.js
+++ assets/scripts/friends-tab.js
@@ -5,16 +5,16 @@ let openFriends = document.getElementById("openFriends");
 
 closeFriends.addEventListener("click", function () {
   console.log("Close friends activity");
-  mainSection.classList.remove("col-8");
-  mainSection.classList.add("col-10");
-  rightSection.classList.add("d-none");
+  mainSection.classList.remove("col-xl-8");
+  mainSection.classList.add("col-xl-10");
+  rightSection.classList.add("d-xl-none");
   openFriends.classList.remove("d-none");
 });
 
 openFriends.addEventListener("click", function () {
   console.log("Open friends activity");
-  mainSection.classList.remove("col-10");
-  mainSection.classList.add("col-8");
-  rightSection.classList.remove("d-none");
+  mainSection.classList.remove("col-xl-10");
+  mainSection.classList.add("col-xl-8");
+  rightSection.classList.remove("d-xl-none");
   openFriends.classList.add("d-none");
 });
